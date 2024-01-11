b6c804043a83f0f3f7f63694cb479de250b851be
duganlx <840797783@qq.com>
2024-01-11 07:14:27 +0000

    aigc取消模型选择&模式选择

--- src/pages/tools/components/aiassistant.tsx
+++ src/pages/tools/components/aiassistant.tsx
@@ -14,8 +14,9 @@ import {
   SmileOutlined,
 } from "@ant-design/icons";
 import { useEmotionCss } from "@ant-design/use-emotion-css";
-import { Select, Input, message } from "antd";
+import { Select, Input, message, Modal } from "antd";
 import React, { useEffect, useRef, useState } from "react";
+import dayjs from "dayjs";
 
 const { TextArea } = Input;
 
@@ -200,16 +201,18 @@ const AiAssistantView: React.FC<AiAssistantViewProps> = (props) => {
   const { layoutsize } = props;
   const [hwins, wwins] = layoutsize;
 
-  // const [mode, setMode] = useState<string>("translation");
-  const [model, setModel] = useState<string>("gpt3.5");
   const [text, setText] = useState<string>("");
   const [QAlist, setQAlist] = useState<QAItem[]>([]);
   const [scrollbottomSign, setScrollbottomSign] = useState<boolean>(false);
   const [progressing, setProgressing] = useState<boolean>(false);
+  const [fullscreen, setFullscreen] = useState<boolean>(false);
+
   const dialogzoneRef = useRef<HTMLDivElement>(null);
+  const modalref = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     ListChat({
+      startTime: dayjs().startOf("day").unix(),
       agentId: ["1559730848930992128_1699580365106"],
       userId: "1559730848930992128",
     }).then((res) => {
@@ -274,6 +277,7 @@ const AiAssistantView: React.FC<AiAssistantViewProps> = (props) => {
           textDecoration: "underline",
         },
       },
+
       ".chat-zone": {
         border: "1px solid #f0f0f0",
         height: "400px",
@@ -359,6 +363,121 @@ const AiAssistantView: React.FC<AiAssistantViewProps> = (props) => {
     };
   });
 
+  const fsclsname = useEmotionCss(() => {
+    return {
+      top: "5vh",
+      margin: "0px 6vw",
+
+      ".ant-modal-content": {
+        width: "86vw",
+        height: "90vh",
+
+        padding: "8px 10px",
+
+        ".ant-modal-header > .ant-modal-title": {
+          fontSize: "18px",
+        },
+
+        ".ant-modal-close": {
+          top: "10px",
+        },
+      },
+
+      ".ant-modal-body": {
+        height: "calc(100% - 40px)",
+        border: "1px solid #f0f0f0",
+      },
+
+      ".modalcontent": {
+        width: "100%",
+        height: "100%",
+
+        ".chat-zone": {
+          border: "1px solid #f0f0f0",
+          height: "100%",
+
+          ".dialog-zone": {
+            height: "calc(100% - 85px)",
+            padding: "5px 13px",
+            overflow: "auto",
+            marginBottom: "5px",
+
+            "&::-webkit-scrollbar": {
+              width: "5px",
+              backgroundColor: "white",
+            },
+            "&::-webkit-scrollbar-thumb": {
+              backgroundColor: "#d9d9d9",
+              borderRadius: "5px",
+            },
+          },
+
+          ".input-zone": {
+            display: "flex",
+            flexDirection: "row",
+            position: "relative",
+            border: "1px solid #d9d9d9",
+            borderRadius: "6px",
+            margin: "0 5px",
+
+            textarea: {
+              "&::-webkit-scrollbar": {
+                width: "5px",
+                backgroundColor: "white",
+              },
+              "&::-webkit-scrollbar-thumb": {
+                backgroundColor: "#d9d9d9",
+                borderRadius: "5px",
+              },
+            },
+
+            "textarea:focus": {
+              boxShadow: "none",
+            },
+
+            ".btn-zone": {
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "flex-end",
+              marginLeft: "6px",
+              marginRight: "5px",
+              marginBottom: "5px",
+
+              ".btn": {
+                width: "30px",
+                height: "30px",
+                borderRadius: "6px",
+                color: progressing ? "black" : "white",
+                backgroundColor: progressing ? "white" : "black",
+                fontSize: "18px",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              },
+
+              ".btn:hover": {
+                cursor: progressing ? "not-allowed" : "pointer",
+              },
+
+              ".progressingbtn": {
+                width: "30px",
+                height: "30px",
+                borderRadius: "6px",
+                color: "white",
+                backgroundColor: "black",
+                fontSize: "18px",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              },
+            },
+          },
+        },
+      },
+    };
+  });
+
   const handleSubmit = () => {
     if (progressing) {
       return;
@@ -393,99 +512,138 @@ const AiAssistantView: React.FC<AiAssistantViewProps> = (props) => {
   };
 
   return (
-    <div className={clsname}>
-      <div className="operbar">
-        {/* <div className="title">mode:</div>
-        <Select
-          size="small"
-          style={{ width: "160px" }}
-          onChange={(value: string) => {
-            setMode(value);
-          }}
-          options={[
-            { value: "translation", label: "Translation" },
-            { value: "FE", label: "Frontend" },
-            { value: "BE", label: "Backend" },
-            { value: "DA", label: "Data Analysis" },
-          ]}
-          value={mode}
-        /> */}
-        <div className="title">model:</div>
-        <Select
-          size="small"
-          style={{ width: "100px" }}
-          onChange={(value: string) => {
-            setModel(value);
-          }}
-          options={[
-            { value: "gpt35", label: "gpt-3.5" },
-            { value: "gpt40", label: "gpt-4" },
-          ]}
-          value={model}
-        />
-        <div className="title">opers:</div>
-        <div
-          className="opitem"
-          onClick={() => {
-            console.log("--1");
-          }}
-        >
-          full-screen
-        </div>
-      </div>
-      <div className="chat-zone">
-        <div ref={dialogzoneRef} className="dialog-zone">
-          {QAlist.map((item, i) => {
-            return (
-              <DialogMessage
-                key={item.question + item.answer}
-                question={item.question}
-                answer={item.answer}
-                loading={item.answer.length == 0}
-                history={item.history}
-                beHistory={() => {
-                  const nQAlist = [...QAlist];
-                  nQAlist[i].history = true;
-                  setQAlist(nQAlist);
-                }}
-              />
-            );
-          })}
-        </div>
-        <div className="input-zone">
-          <TextArea
-            rows={3}
-            value={text}
-            style={{
-              border: 0,
-              resize: "none",
+    <>
+      <div className={clsname}>
+        <div className="operbar">
+          <div className="title">opers:</div>
+          <div
+            className="opitem"
+            onClick={() => {
+              setFullscreen(true);
             }}
-            onChange={(eve) => setText(eve.target.value)}
-            onKeyDown={(eve) => {
-              if (eve.key === "Enter") {
-                if (eve.ctrlKey) {
-                  setText((prevText) => prevText + "\n");
-                } else {
-                  eve.preventDefault();
-                  handleSubmit();
-                }
-              }
-            }}
-            disabled={progressing}
-          />
-          <div className="btn-zone">
-            <div
-              className="btn"
-              onClick={() => {
-                handleSubmit();
+          >
+            full-screen
+          </div>
+        </div>
+        <div className="chat-zone">
+          <div ref={dialogzoneRef} className="dialog-zone">
+            {QAlist.map((item, i) => {
+              return (
+                <DialogMessage
+                  key={item.question + item.answer}
+                  question={item.question}
+                  answer={item.answer}
+                  loading={item.answer.length == 0}
+                  history={item.history}
+                  beHistory={() => {
+                    const nQAlist = [...QAlist];
+                    nQAlist[i].history = true;
+                    setQAlist(nQAlist);
+                  }}
+                />
+              );
+            })}
+          </div>
+          <div className="input-zone">
+            <TextArea
+              rows={3}
+              value={text}
+              style={{
+                border: 0,
+                resize: "none",
               }}
-            >
-              {progressing ? <PauseCircleOutlined /> : <ArrowUpOutlined />}
+              onChange={(eve) => setText(eve.target.value)}
+              onKeyDown={(eve) => {
+                if (eve.key === "Enter") {
+                  if (eve.ctrlKey) {
+                    setText((prevText) => prevText + "\n");
+                  } else {
+                    eve.preventDefault();
+                    handleSubmit();
+                  }
+                }
+              }}
+              disabled={progressing}
+            />
+            <div className="btn-zone">
+              <div
+                className="btn"
+                onClick={() => {
+                  handleSubmit();
+                }}
+              >
+                {progressing ? <PauseCircleOutlined /> : <ArrowUpOutlined />}
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+      <Modal
+        className={fsclsname}
+        title={"AI Assistant"}
+        open={fullscreen}
+        onCancel={() => {
+          setFullscreen(false);
+        }}
+        footer={null}
+        destroyOnClose={true}
+      >
+        <div className="modalcontent" ref={modalref}>
+          <div className="chat-zone">
+            <div ref={dialogzoneRef} className="dialog-zone">
+              {QAlist.map((item, i) => {
+                return (
+                  <DialogMessage
+                    key={item.question + item.answer}
+                    question={item.question}
+                    answer={item.answer}
+                    loading={item.answer.length == 0}
+                    history={item.history}
+                    beHistory={() => {
+                      const nQAlist = [...QAlist];
+                      nQAlist[i].history = true;
+                      setQAlist(nQAlist);
+                    }}
+                  />
+                );
+              })}
+            </div>
+            <div className="input-zone">
+              <TextArea
+                rows={3}
+                value={text}
+                style={{
+                  border: 0,
+                  resize: "none",
+                }}
+                onChange={(eve) => setText(eve.target.value)}
+                onKeyDown={(eve) => {
+                  if (eve.key === "Enter") {
+                    if (eve.ctrlKey) {
+                      setText((prevText) => prevText + "\n");
+                    } else {
+                      eve.preventDefault();
+                      handleSubmit();
+                    }
+                  }
+                }}
+                disabled={progressing}
+              />
+              <div className="btn-zone">
+                <div
+                  className="btn"
+                  onClick={() => {
+                    handleSubmit();
+                  }}
+                >
+                  {progressing ? <PauseCircleOutlined /> : <ArrowUpOutlined />}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Modal>
+    </>
   );
 };
 
