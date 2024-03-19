37ad918617c400b3e8e62ba148af577d85a3cf50
duganlx <840797783@qq.com>
2024-03-19 09:38:46 +0000

    [trial] 确定下一个研究课题：自适应

--- src/pages/trial/index.tsx
+++ src/pages/trial/index.tsx
@@ -1,5 +1,27 @@
+import { useEmotionCss } from "@ant-design/use-emotion-css";
+
 const TrialView: React.FC = () => {
-  return <></>;
+  console.log("--1");
+
+  const clsname = useEmotionCss(() => {
+    return {
+      height: "300px",
+      width: "300px",
+      backgroundColor: "#ff9c6e",
+
+      ".view": {
+        overflow: "hidden",
+        whiteSpace: "nowrap",
+        textOverflow: "ellipsis",
+      },
+    };
+  });
+
+  return (
+    <div className={clsname}>
+      <div className="view">xxxxxxxxxxxxx</div>
+    </div>
+  );
 };
 
 export default TrialView;
