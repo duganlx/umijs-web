576e07e8a73036358c31c464ae6bb1d562973884
duganlx <840797783@qq.com>
2024-03-24 03:50:40 +0000

    整合 echarts

--- src/pages/trial/index.tsx
+++ src/pages/trial/index.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactGridLayoutDemo from "./components/reactGridLayoutDemo";
 import GenesisDemo from "./components/genesisDemo";
+import KLineChart from "./components/kLineDemo";
 
 const TrialView: React.FC = () => {
   return (
     <>
-      <GenesisDemo />
+      {/* <ReactGridLayoutDemo /> */}
+      {/* <GenesisDemo /> */}
+      <KLineChart />
     </>
   );
 };
